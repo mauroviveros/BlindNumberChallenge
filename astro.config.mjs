1677eb07978f893370a12771aa643f2457565e86
Mauro Daniel Viveros <maurod.viveros@gmail.com>
2023-05-17 14:25:56 -0300

    fix: :truck: rename config base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/blindnumberchallenge'
+    base: '/blind_number_challenge'
 });
